01c5fed9678f9f244eaa577cc17f6d44ff3c1368
Iftikhar Khan <iakhan4425@gmail.com>
2018-03-21 19:36:55 -0400

    Added CMakeFile and IDE config

---
+++ cmake-build-debug/CMakeFiles/Project_Evolution.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_Evolution.dir/main.cpp.o"
+  "Project_Evolution.pdb"
+  "Project_Evolution"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_Evolution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
